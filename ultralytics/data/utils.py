7f50ba116df3f6a085f44346e6999ba9847d4832
ZahraShahmohamadi <zahra.shahmohamadi02@gmail.com>
2025-10-09 16:37:36 +0330

    Update utils.py
    Signed-off-by: ZahraShahmohamadi <zahra.shahmohamadi02@gmail.com>

--- ultralytics/data/utils.py
+++ ultralytics/data/utils.py
@@ -35,6 +35,7 @@ from ultralytics.utils.downloads import download, safe_download, unzip_file
 from ultralytics.utils.ops import segments2boxes
 
 HELP_URL = "See https://docs.ultralytics.com/datasets for dataset formatting guidance."
+DATASET_CACHE_VERSION = "1.0.3"  # Ultralytics dataset cache version
 IMG_FORMATS = {"bmp", "dng", "jpeg", "jpg", "mpo", "png", "tif", "tiff", "webp", "pfm", "heic", "npy"}  # image suffixes
 VID_FORMATS = {"asf", "avi", "gif", "m4v", "mkv", "mov", "mp4", "mpeg", "mpg", "ts", "wmv", "webm"}  # video suffixes
 PIN_MEMORY = str(os.getenv("PIN_MEMORY", not MACOS)).lower() == "true"  # global pin_memory for dataloaders
